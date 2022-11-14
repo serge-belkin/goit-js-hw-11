f21d146b0ecee9c5237c9a998cb59ac4970015ea
serge-belkin <cer_fi@yahoo.com>
2022-11-14 20:27:49 +0200

    1

--- src/js/fetchImages.js
+++ src/js/fetchImages.js
@@ -1,6 +1,6 @@
 export const fetchImages = async (inputValue, pageNr) => {
   return await fetch(
-    `https://pixabay.com/api/?key=29588079-fbc492831fdad231bf7222b96&q=${inputValue}&orientation=horizontal&safesearch=true&image_type=photo&per_page=40&page=${pageNr}`
+    `https://pixabay.com/api/?key=31325871-0cde844d39520f6c11f9b55e1&q=${inputValue}&orientation=horizontal&safesearch=true&image_type=photo&per_page=40&page=${pageNr}`
   )
     .then(async response => {
       if (!response.ok) {
